368e2735daf55cd51c105d0354675e42c79c1cbc
zlyuancn <zlyuancn@163.com>
2021-02-05 10:28:14 +0800

    提供注册将apollo的命名空间指定key解析为json

--- config/apollo.go
+++ config/apollo.go
@@ -21,21 +21,25 @@ import (
 	"github.com/zly-app/zapp/logger"
 )
 
-// 分析apollo配置各命名空间的key等级
-const (
-	analyseApolloConfigFrameKeyLevel      = 1 // frame
-	analyseApolloConfigServicesKeyLevel   = 1 // services
-	analyseApolloConfigComponentsKeyLevel = 2 // components
-)
-
-// 分析apollo配置各命名空间的key前缀
-const (
-	analyseApolloConfigFrameKeyPrefixes      = "Log,Labels" // frame
-	analyseApolloConfigServicesKeyPrefixes   = "*"          // services
-	analyseApolloConfigComponentsKeyPrefixes = "*"          // components
-)
-
 type ApolloConfig = apollo_sdk.ApolloConfig
+
+// 将命名空间配置的指定等级及主key的value解析为json, *匹配任何主key
+var parseConfigToJsonOfPrimaryKeys = map[string]struct {
+	primaryKey string
+	keyLevel   int
+}{
+	apollo_sdk.FrameNamespace:      {"log,labels", 1},
+	apollo_sdk.ServicesNamespace:   {"*", 1},
+	apollo_sdk.ComponentsNamespace: {"*", 2},
+}
+
+// 注册将命名空间配置的哪些key解析为json, *匹配任何主key
+func RegistryParseConfigToJsonOfPrimaryKey(namespace, primaryKey string, keyLevel int) {
+	parseConfigToJsonOfPrimaryKeys[namespace] = struct {
+		primaryKey string
+		keyLevel   int
+	}{primaryKey: primaryKey, keyLevel: keyLevel}
+}
 
 // 从viper构建apollo配置
 func makeApolloConfigFromViper(vi *viper.Viper) (*ApolloConfig, error) {
@@ -53,16 +57,9 @@ func makeViperFromApollo(conf *ApolloConfig) (*viper.Viper, error) {
 
 	configs := make(map[string]interface{}, len(data))
 	for namespace, raw := range data {
-		data := map[string]interface{}(raw)
-		switch namespace {
-		case apollo_sdk.FrameNamespace:
-			data = analyseApolloConfig(namespace, raw, analyseApolloConfigFrameKeyLevel, analyseApolloConfigFrameKeyPrefixes)
-		case apollo_sdk.ServicesNamespace:
-			data = analyseApolloConfig(namespace, raw, analyseApolloConfigServicesKeyLevel, analyseApolloConfigServicesKeyPrefixes)
-		case apollo_sdk.ComponentsNamespace:
-			data = analyseApolloConfig(namespace, raw, analyseApolloConfigComponentsKeyLevel, analyseApolloConfigComponentsKeyPrefixes)
-		}
-		configs[strings.ReplaceAll(namespace, "_", "")] = data
+		d := map[string]interface{}(raw)
+		d = analyseApolloConfig(namespace, d)
+		configs[namespace] = d
 	}
 
 	// 构建viper
@@ -74,21 +71,26 @@ func makeViperFromApollo(conf *ApolloConfig) (*viper.Viper, error) {
 }
 
 // 分析apollo配置, 它会匹配key前缀且key的层级数值为level, 然后将value转为 map[string]interface{}
-func analyseApolloConfig(namespace string, raw map[string]interface{}, level int, prefixes string) map[string]interface{} {
+func analyseApolloConfig(namespace string, raw map[string]interface{}) map[string]interface{} {
+	primaryKeys, ok := parseConfigToJsonOfPrimaryKeys[namespace]
+	if !ok {
+		return raw
+	}
+
 	prefixMap := make(map[string]bool)
-	for _, prefix := range strings.Split(strings.ToLower(prefixes), ",") {
+	for _, prefix := range strings.Split(strings.ToLower(primaryKeys.primaryKey), ",") {
 		prefixMap[prefix] = true
 	}
 
 	data := make(map[string]interface{})
 	for key, value := range raw {
 		keys := strings.Split(key, ".")
-		if len(keys) != level { // 不匹配等级
+		if len(keys) != primaryKeys.keyLevel { // 不匹配等级
 			data[key] = value
 			continue
 		}
 
-		if prefixes != "*" && !prefixMap[strings.ToLower(keys[0])] { // 不匹配前缀
+		if primaryKeys.primaryKey != "*" && !prefixMap[strings.ToLower(keys[0])] { // 不匹配主key
 			data[key] = value
 			continue
 		}
