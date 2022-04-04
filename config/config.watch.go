5b2b01b681f4acc5faf1041c0699d61ee2589031
zlyuancn <zlyuancn@163.com>
2022-04-04 21:47:22 +0800

    配置观察

--- config/config.watch.go
+++ config/config.watch.go
@@ -1,37 +1,10 @@
 package config
 
 import (
-	"go.uber.org/zap"
-
 	"github.com/zly-app/zapp/core"
-	"github.com/zly-app/zapp/logger"
 )
 
-// 用于承载配置观察提供者
-var configWatchProviders = make(map[string]core.IConfigWatchProvider)
-
-// 获取默认配置观察提供者, 可能返回nil
-func GetDefaultConfigWatchProvider() core.IConfigWatchProvider {
-	return configWatchProviders["default"]
-}
-
-// 设置默认配置观察提供者
-func SetDefaultConfigWatchProvider(p core.IConfigWatchProvider) {
-	configWatchProviders["default"] = p
-}
-
-// 添加配置观察提供者, 第一个被添加的提供者会作为默认提供者
-func AddConfigWatchProvider(name string, p core.IConfigWatchProvider) {
-	if name == "default" {
-		logger.Log.Fatal("配置提供者名不能为default")
-	}
-	if _, ok := configWatchProviders[name]; ok {
-		logger.Log.Fatal("配置提供者已存在", zap.String("name", name))
-	}
-	configWatchProviders[name] = p
-
-	// 设置默认配置提供者
-	if GetDefaultConfigWatchProvider() == nil {
-		SetDefaultConfigWatchProvider(p)
-	}
+// 观察key
+func (c *configCli) WatchKey(groupName, keyName string, opts ...core.ConfigWatchOption) core.IConfigWatchKeyObject {
+	panic("implement me")
 }
