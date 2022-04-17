c36e58a7dc832a8953450e83870e04d959a33c33
zlyuancn <zlyuancn@163.com>
2022-04-17 14:43:33 +0800

    zlog的hook支持启动hook回调

--- pkg/zlog/hook.go
+++ pkg/zlog/hook.go
@@ -17,18 +17,9 @@ type interceptorFunc func(ent *zapcore.Entry, fields []zapcore.Field) (cancel bo
 
 type interceptor struct {
 	zapcore.Core
-	funcs []interceptorFunc
+	conf *hookConfig
 }
 
-// 拦截器
-func WithHook(funcs ...interceptorFunc) zap.Option {
-	return zap.WrapCore(func(core zapcore.Core) zapcore.Core {
-		return &interceptor{
-			Core:  core,
-			funcs: append(([]interceptorFunc)(nil), funcs...),
-		}
-	})
-}
 func (c *interceptor) Check(ent zapcore.Entry, ce *zapcore.CheckedEntry) *zapcore.CheckedEntry {
 	if c.Enabled(ent.Level) {
 		return ce.AddCore(ent, c)
@@ -37,15 +28,65 @@ func (c *interceptor) Check(ent zapcore.Entry, ce *zapcore.CheckedEntry) *zapcor
 }
 func (h *interceptor) With(fields []zapcore.Field) zapcore.Core {
 	return &interceptor{
-		Core:  h.Core.With(fields),
-		funcs: h.funcs,
+		Core: h.Core.With(fields),
+		conf: h.conf,
 	}
 }
 func (c *interceptor) Write(ent zapcore.Entry, fields []zapcore.Field) error {
-	for i := range c.funcs {
-		if c.funcs[i](&ent, fields) {
+	for _, fn := range c.conf.fns {
+		if fn(&ent, fields) {
 			return nil
 		}
 	}
 	return c.Core.Write(ent, fields)
+}
+
+func (i *interceptor) WrapCore(core zapcore.Core) zapcore.Core {
+	if i.Core != nil {
+		return i
+	}
+	i.Core = core
+
+	for _, fn := range i.conf.startHookCallbacks {
+		fn()
+	}
+	return i
+}
+
+// 拦截器
+func WithHook(fns ...interceptorFunc) zap.Option {
+	conf := NewHookConfig()
+	conf.AddInterceptorFunc(fns...)
+	return WithHookByConfig(conf)
+}
+
+// 拦截器
+func WithHookByConfig(conf *hookConfig) zap.Option {
+	i := &interceptor{
+		conf: conf,
+	}
+	return zap.WrapCore(func(core zapcore.Core) zapcore.Core {
+		return i.WrapCore(core)
+	})
+}
+
+type hookConfig struct {
+	fns                []interceptorFunc
+	startHookCallbacks []func()
+}
+
+// 添加开始hook回调
+func (h *hookConfig) AddStartHookCallbacks(callbacks ...func()) *hookConfig {
+	h.startHookCallbacks = append(h.startHookCallbacks, callbacks...)
+	return h
+}
+
+// 添加拦截器函数
+func (h *hookConfig) AddInterceptorFunc(fns ...interceptorFunc) *hookConfig {
+	h.fns = append(h.fns, fns...)
+	return h
+}
+
+func NewHookConfig() *hookConfig {
+	return &hookConfig{}
 }
