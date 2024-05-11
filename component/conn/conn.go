f46e0cb5f72d16596573af28f945ce90c80e67de
zlyuanzhang <zlyuanzhang@tencent.com>
2024-05-11 14:10:45 +0800

    conn

--- component/conn/conn.go
+++ component/conn/conn.go
@@ -43,6 +43,7 @@ func NewConn() *Conn {
 }
 
 // 获取实例
+// Deprecated: use GetConn
 func (c *Conn) GetInstance(creator CreatorFunc, name ...string) IInstance {
 	if len(name) == 0 {
 		return c.getInstance(creator, consts.DefaultComponentName)
@@ -99,6 +100,59 @@ func (c *Conn) getInstance(creator CreatorFunc, name string) IInstance {
 
 	wg.wg.Done()
 	return wg.instance
+}
+
+// 获取conn
+func (c *Conn) GetConn(creator CreatorFunc, name ...string) (IInstance, error) {
+	if len(name) == 0 {
+		return c.getConn(creator, consts.DefaultComponentName)
+	}
+	return c.getConn(creator, name[0])
+}
+
+func (c *Conn) getConn(creator CreatorFunc, name string) (IInstance, error) {
+	c.mx.RLock()
+	wg, ok := c.wgs[name]
+	c.mx.RUnlock()
+
+	if ok {
+		wg.wg.Wait()
+		return wg.instance, wg.e
+	}
+
+	c.mx.Lock()
+
+	// 再获取一次, 它可能在获取锁的过程中完成了
+	if wg, ok = c.wgs[name]; ok {
+		c.mx.Unlock()
+		wg.wg.Wait()
+		return wg.instance, wg.e
+	}
+
+	// 占位置
+	wg = new(connWaitGroup)
+	wg.wg.Add(1)
+	c.wgs[name] = wg
+	c.mx.Unlock()
+
+	var err error
+	err = utils.Recover.WrapCall(func() error {
+		wg.instance, err = creator(name)
+		return err
+	})
+
+	// 如果出现错误, 删除占位
+	if err != nil {
+		wg.e = err
+		wg.wg.Done()
+		c.mx.Lock()
+		delete(c.wgs, name)
+		c.mx.Unlock()
+		return nil, err
+	}
+
+	wg.wg.Done()
+	return wg.instance, nil
 }
 
 // 移除实例, 移除时会关闭示例
