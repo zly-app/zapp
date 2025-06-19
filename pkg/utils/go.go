560e206b87148386bf105b6aed745a7b147dcf87
zlyuancn <zlyuancn@163.com>
2025-06-19 19:33:09 +0800

    提供 GoRetWait

--- pkg/utils/go.go
+++ pkg/utils/go.go
@@ -35,3 +35,34 @@ func (goCli) GoAndWait(fns ...func() error) error {
 	}
 	return err
 }
+
+// 启用协程运行函数, 并返回一个wait函数等待所有函数执行完成, 会自动 Recover, 如果有函数执行错误, 会返回第一个不为nil的error
+func (goCli) GoRetWait(fns ...func() error) func() error {
+	if len(fns) == 0 {
+		return func() error { return nil }
+	}
+
+	var wg sync.WaitGroup
+	errChan := make(chan error, len(fns))
+
+	for _, fn := range fns {
+		wg.Add(1)
+		go func(fn func() error) {
+			defer wg.Done()
+			if e := Recover.WrapCall(fn); e != nil {
+				errChan <- e
+			}
+		}(fn)
+	}
+
+	return func() error {
+		wg.Wait()
+
+		var err error
+		select {
+		case err = <-errChan:
+		default:
+		}
+		return err
+	}
+}
