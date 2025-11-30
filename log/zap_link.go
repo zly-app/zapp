e00d4d1ce2e0cc8d46bb8e6f0627960fa6c46b4a
zlyuancn <zlyuancn@163.com>
2025-11-30 19:33:53 +0800

    log提供zap.String这种快捷方式

---
+++ log/zap_link.go
@@ -0,0 +1,53 @@
+package log
+
+import (
+	"go.uber.org/zap"
+)
+
+var Binary = zap.Binary
+var Bool = zap.Bool
+var Boolp = zap.Boolp
+var ByteString = zap.ByteString
+var Complex128 = zap.Complex128
+var Complex128p = zap.Complex128p
+var Complex64 = zap.Complex64
+var Complex64p = zap.Complex64p
+var Float64 = zap.Float64
+var Float64p = zap.Float64p
+var Float32 = zap.Float32
+var Float32p = zap.Float32p
+var Int = zap.Int
+var Intp = zap.Intp
+var Int64 = zap.Int64
+var Int64p = zap.Int64p
+var Int32 = zap.Int32
+var Int32p = zap.Int32p
+var Int16 = zap.Int16
+var Int16p = zap.Int16p
+var Int8 = zap.Int8
+var Int8p = zap.Int8p
+var String = zap.String
+var Stringp = zap.Stringp
+var Uint = zap.Uint
+var Uintp = zap.Uintp
+var Uint64 = zap.Uint64
+var Uint64p = zap.Uint64p
+var Uint32 = zap.Uint32
+var Uint32p = zap.Uint32p
+var Uint16 = zap.Uint16
+var Uint16p = zap.Uint16p
+var Uint8 = zap.Uint8
+var Uint8p = zap.Uint8p
+var Uintptr = zap.Uintptr
+var Uintptrp = zap.Uintptrp
+var Reflect = zap.Reflect
+var Namespace = zap.Namespace
+var Stringer = zap.Stringer
+var Time = zap.Time
+var Timep = zap.Timep
+var Stack = zap.Stack
+var StackSkip = zap.StackSkip
+var Duration = zap.Duration
+var Durationp = zap.Durationp
+var Object = zap.Object
+var Any = zap.Any
